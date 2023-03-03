32ce6c1f41af7c4f58eb16b819733177ea8f83ef
Daniil Rassadin <sweet.danen@yandex.ru>
2023-03-03 19:47:12 +0300

    initial

---
+++ Homework19/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Homework19
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
