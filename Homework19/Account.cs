619f30d79d02a92fbcd6a61c60c27439b4478a53
Daniil Rassadin <sweet.danen@yandex.ru>
2023-03-04 15:38:40 +0300

    account class added

---
+++ Homework19/Account.cs
@@ -0,0 +1,30 @@
+﻿namespace Homework19
+{
+    public class Account
+    {
+        public int Id;
+
+        public string Surname;
+
+        public string Name;
+
+        public string Midname;
+
+        public int Phone;
+
+        public string Address;
+
+        public string Description;
+
+        public Account(int id, string surname, string name, string midname, int phone, string address, string description)
+        {
+            Id = id;
+            Surname = surname;
+            Name = name;
+            Midname = midname;
+            Phone = phone;
+            Address = address;
+            Description = description;
+        }
+    }
+}
