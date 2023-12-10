b6861634d896a4d3d5ca3f42e6913f5d55758f10
Daniil Rassadin <sweet.danen@yandex.ru>
2023-12-10 11:00:17 +0300

    i ve added webApi in my project

--- ContactWebAPI/obj/Debug/net8.0/ContactWebAPI.AssemblyInfo.cs
+++ ContactWebAPI/obj/Debug/net8.0/ContactWebAPI.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Homework19")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ContactWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Homework19")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Homework19")]
+[assembly: System.Reflection.AssemblyProductAttribute("ContactWebAPI")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ContactWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
