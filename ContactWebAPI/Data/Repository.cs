ef5591be386e6e0cf549393ec785d4aaed34f8f9
Daniil Rassadin <sweet.danen@yandex.ru>
2023-12-19 20:26:22 +0300

    api and asp projects are ready, now its wpf time

--- ContactWebAPI/Data/Repository.cs
+++ ContactWebAPI/Data/Repository.cs
@@ -24,21 +24,47 @@ namespace ContactWebAPI.Data
 
 		public static void AddContact(Contact contact) 
 		{
-			contact.Id = data.Count;
+			contact.Id = data.Count+1;
 			data.Add(contact);
-			//context.SaveChanges();
+			context.SaveChanges();
+			//Save();
 		}
 
 		public static void UpdateContact(Contact contact)
 		{
-			data.Insert(contact.Id-1, contact);
-			//context.SaveChanges();
+			data[contact.Id-1] = contact;
+			context.SaveChanges();
+			//Save();
 		}
 
 		public static void RemoveContact(int id)
 		{
 			data.RemoveAt(id);
-			//context.SaveChanges();
+			context.SaveChanges();
+			//Save();
+		}
+
+		public static void Save()
+		{
+			foreach(Contact contact in data)
+			{
+				if(context.Contacts.Contains(contact)) 
+				{
+					context.Contacts.Update(contact);
+				}
+				else 
+				{
+					context.Contacts.Add(contact); 
+				}
+			}
+
+			using(var trans = context.Database.BeginTransaction())
+			{
+				context.Database.ExecuteSql($"SET IDENTITY_INSERT [dbo].[Contacts] ON");
+				context.SaveChanges();
+				context.Database.ExecuteSql($"SET IDENTITY_INSERT [dbo].[Contacts] OFF");
+				trans.Commit();
+			}
 		}
 	}
 }
--- MyHomework20/obj/Debug/net8.0/MyHomework20.AssemblyInfo.cs
+++ MyHomework20/obj/Debug/net8.0/MyHomework20.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyHomework20")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e47f8abb8bf2f081933c409841dc3c8cad2ce4d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a33b6a5c2e83ff43f1d181529f00c8063f187a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyHomework20")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyHomework20")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
